a195ad61e9e3504a292b01333f333265c1cd7936
ling976432 <82493590+ling976432@users.noreply.github.com>
2023-01-18 11:53:29 +0100

    update Astar

--- aStar test/AStar.c
+++ aStar test/AStar.c
@@ -16,12 +16,23 @@ char map[map_size_rows][map_size_cols] = {
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
+// char map[map_size_rows][map_size_cols] = {
+//     {-2, -2, -2, -2, -2, -2, -2, -2, -2, -2},
+//     {-2,  0, -1,  0, -1,  0, -1,  0, -1, -2},
+//     {-2, -1, -1, -1, -1, -1, -1, -1, -1, -2},
+//     {-2,  0, -1,  0, -1,  0, -1,  0, -1, -2},
+//     {-2, -1, -1, -1, -1, -1, -1, -1, -1, -2},
+//     {-2,  0, -1,  0, -1,  0, -1,  0, -1, -2},
+//     {-2, -1, -1, -1, -1, -1, -1, -1, -1, -2},
+//     {-2,  0, -1,  0, -1,  0, -1,  0, -1, -2},
+//     {-2, -1, -1, -1, -1, -1, -1, -1, -1, -2},
+//     {-2, -2, -2, -2, -2, -2, -2, -2, -2, -2}};
 /* description of graph node */
 struct stop
 {
@@ -29,11 +40,9 @@ struct stop
     /* array of indexes of routes from this stop to neighbours in array of all routes */
     int *n;
     int n_len;
-    double f, g, h;//cout total, distance de l'arrivé, distance du départ 
+    double f, g, h; // cout total, distance de l'arrivé, distance du départ
     int from;
 };
-
-
 
 int ind[map_size_rows][map_size_cols] = {
     {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
@@ -48,7 +57,8 @@ int ind[map_size_rows][map_size_cols] = {
     {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1}};
 
 /* description of route between two nodes */
-struct route
+struct route//trace le chemin entre les deux buts
+
 {
     /* route has only one direction! */
     int x; /* index of stop in array of all stops of src of this route */
@@ -56,15 +66,16 @@ struct route
     double d;
 };
 
-void freestop(struct stop s){
-    s.col=0;
-    s.row=0;
-    s.n=NULL;
-    s.n_len=NULL;
-    s.f=DBL_MAX;
-    s.g=DBL_MAX;
-    s.h=DBL_MAX;
-    s.from=0;    
+void freestop(struct stop s)
+{
+    s.col = 0;
+    s.row = 0;
+    s.n = NULL;
+    s.n_len = NULL;
+    s.f = DBL_MAX;
+    s.g = DBL_MAX;
+    s.h = DBL_MAX;
+    s.from = 0;
 }
 
 int main()
@@ -90,18 +101,18 @@ int main()
     {
         for (j = 1; j < map_size_cols - 1; j++)
         {
-            if (!map[i][j])
-            {                                                                       // 0 on rentre dans le if 1 on rentr pas
-                ++s_len;                                                            // augmente l'indice de la taille du nombre de stop possible
+            if (map[i][j]!=1)
+            {
+                printf("a"); // 0 on rentre dans le if 1 on rentr pas
+                ++s_len;
+                // augmente l'indice de la taille du nombre de stop possible
                 stops = (struct stop *)realloc(stops, s_len * sizeof(struct stop)); // augmente la taille du tableau de stop
                 int t = s_len - 1;
                 stops[t].col = j;
                 stops[t].row = i;
-                
-                
-                    
+
                 stops[t].from = -1;
-                stops[t].g = DBL_MAX;//comprend pas
+                stops[t].g = DBL_MAX; // comprend pas
                 stops[t].n_len = 0;
                 stops[t].n = NULL;
                 ind[i][j] = t;
@@ -110,10 +121,9 @@ int main()
                 //     --s_len;
 
                 //     stops = (struct stop *)realloc(stops, s_len * sizeof(struct stop));
-                    
-                    
+
                 // }
-                 // affecte a ind(i)(j) la valeur t = s_len-1
+                // affecte a ind(i)(j) la valeur t = s_len-1
             }
         }
     }
@@ -126,37 +136,50 @@ int main()
     // Distance entre case stop(e)(l'arrivé) et case courante(i) stocker dans stop.h(i)
     for (i = 0; i < s_len; i++)
     {
-        /*nouvel formule : stops[i].h = (stops[e].row - stops[i].row) + (stops[e].col - stops[i].col);
-        */stops[i].h = sqrt(pow(stops[e].row - stops[i].row, 2) + pow(stops[e].col - stops[i].col, 2)); //distance de l'arrivé de tous les points de stops
+         stops[i].h = (stops[e].row - stops[i].row) + (stops[e].col - stops[i].col);
+         
+        // stops[i].h = sqrt(pow(stops[e].row - stops[i].row, 2) + pow(stops[e].col - stops[i].col, 2)); // distance de l'arrivé de tous les points de stops
     }
 
-    for (i = 1; i < map_size_rows - 1; i++)
+    for (i = 2; i < map_size_rows - 1; i++)
     {
-        for (j = 1; j < map_size_cols - 1; j++)
+        for (j = 2; j < map_size_cols - 1; j++)
         {
-            if (ind[i][j] >= 0)//verifie si c'est une case deja visité par stop où déplacement possible
+            if (ind[i][j] >= 0) // verifie si c'est une case deja visité par stop où déplacement possible
             {
-                for (k = i - 1; k <= i + 1; k++)
+                for (k = i - 1; k <= i + 1; k+=2)//gère quel type de déplacement est possible diagonale uniquement par es coté etc.
                 {
-                    for (l = j - 1; l <= j + 1; l++)
+                    for (l = j - 1; l <= j + 1; l+=2)
                     {
-                        if ((k == i) and (l == j))
+                         if ((k == i) and (l == j))
+                         {
+                             continue;
+                         }
+                        // if (abs(i-k)+abs(j-l)==2)
+                        // {
+                             if (ind[k][l] >= 0) // incompréhensible
                         {
-                            continue;
-                        }
-                        if (ind[k][l] >= 0)//incompréhensible
-                        {
+                            printf("b");
                             ++r_len;
                             routes = (struct route *)realloc(routes, r_len * sizeof(struct route));
                             int t = r_len - 1;
                             routes[t].x = ind[i][j];
                             routes[t].y = ind[k][l];
-                            /*nouvelle formule à utiliser ? : routes[t].d = (stops[routes[t].y].row - stops[routes[t].x].row) + (stops[routes[t].y].col - stops[routes[t].x].col);
-                            */routes[t].d = sqrt(pow(stops[routes[t].y].row - stops[routes[t].x].row, 2) + pow(stops[routes[t].y].col - stops[routes[t].x].col, 2));//distance entre 
+                            routes[t].d = (stops[routes[t].y].row - stops[routes[t].x].row) + (stops[routes[t].y].col - stops[routes[t].x].col);
+                           
+                            // routes[t].d = sqrt(pow(stops[routes[t].y].row - stops[routes[t].x].row, 2) + pow(stops[routes[t].y].col - stops[routes[t].x].col, 2)); // distance entre
                             ++stops[routes[t].x].n_len;
                             stops[routes[t].x].n = (int *)realloc(stops[routes[t].x].n, stops[routes[t].x].n_len * sizeof(int));
                             stops[routes[t].x].n[stops[routes[t].x].n_len - 1] = t;
                         }
+                        // }
+                        
+                        
+                        if ((k == i) and (l == j))
+                         {
+                             continue;
+                         }
+                       
                     }
                 }
             }
@@ -164,34 +187,34 @@ int main()
     }
 
     open[0] = s;
-    stops[s].g = 0;//distance du départ et .h distance de l'arrivé
-    stops[s].f = stops[s].g + stops[s].h;//coût total des chemin
-    found = 0;//test si on est arruvé a destination
+    stops[s].g = 0;                       // distance du départ et .h distance de l'arrivé
+    stops[s].f = stops[s].g + stops[s].h; // coût total des chemin
+    found = 0;                            // test si on est arruvé a destination
 
-    while (o_len and not found)//tant que o_len =! NULL et found =! 1
+    while (o_len and not found) // tant que o_len =! NULL et found =! 1
     {
-        min = DBL_MAX; //permet d'avoir la valeur max des doubles et donc tout nombre comparé a min pour la première fois prendra forcément la place de min.
+        min = DBL_MAX; // permet d'avoir la valeur max des doubles et donc tout nombre comparé a min pour la première fois prendra forcément la place de min.
 
         for (i = 0; i < o_len; i++)
         {
-            if (stops[open[i]].f < min )//regarde si le node qu'on regarde à un coût plus faible que celui actuel
+            if (stops[open[i]].f < min) // regarde si le node qu'on regarde à un coût plus faible que celui actuel
             {
-                
-                current = open[i];   //continue le deplacement du snake vers le coût le plus faible.
+
+                current = open[i]; // continue le deplacement du snake vers le coût le plus faible.
                 min = stops[open[i]].f;
             }
         }
 
-        if (current == e)//regarde si on est arrivé à destination 
+        if (current == e) // regarde si on est arrivé à destination
         {
-            found = 1;//definé qu'on a trouvé
+            found = 1; // definé qu'on a trouvé
 
             ++p_len;
             path = (int *)realloc(path, p_len * sizeof(int));
             path[p_len - 1] = current;
             while (stops[current].from >= 0)
             {
-                current = stops[current].from;//retrace le chemin dans le sens inverse (je crois)
+                current = stops[current].from; // retrace le chemin dans le sens inverse (je crois)
                 ++p_len;
                 path = (int *)realloc(path, p_len * sizeof(int));
                 path[p_len - 1] = current;
@@ -251,12 +274,11 @@ int main()
                 }
             }
 
-            if (b or (tempg < stops[routes[stops[current].n[i]].y].g))//??????????????????????????????????????
+            if (b or (tempg < stops[routes[stops[current].n[i]].y].g)) //??????????????????????????????????????
             {
                 stops[routes[stops[current].n[i]].y].from = current;
                 stops[routes[stops[current].n[i]].y].g = tempg;
                 stops[routes[stops[current].n[i]].y].f = stops[routes[stops[current].n[i]].y].g + stops[routes[stops[current].n[i]].y].h;
-                
 
                 if (b)
                 {
@@ -267,8 +289,8 @@ int main()
             }
         }
     }
-    //print board ind
-     for (i = 0; i < map_size_rows; i++)
+    // print board ind
+    for (i = 0; i < map_size_rows; i++)
     {
         for (j = 0; j < map_size_cols; j++)
         {
@@ -278,9 +300,8 @@ int main()
             }
             else
             {
-               
-                    putchar('.');
-                
+
+                putchar('.');
             }
         }
         putchar('\n');
@@ -329,13 +350,11 @@ int main()
         for (i = p_len - 1; i >= 0; i--)
         {
             printf("(%1.0f, %1.0f)\n", stops[path[i]].col, stops[path[i]].row);
-        //     if (i>2)
-        //     {
-        //          printf("(%1.0f)\n",stops[path[i]].f-stops[path[i-2]].f);
-        // }
-            }
-            
-           
+            //     if (i>2)
+            //     {
+            //          printf("(%1.0f)\n",stops[path[i]].f-stops[path[i-2]].f);
+            // }
+        }
     }
 
     for (i = 0; i < s_len; ++i)
@@ -348,6 +367,5 @@ int main()
     free(open);
     free(closed);
 
-    
     return 0;
 }
